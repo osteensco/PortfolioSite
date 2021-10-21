694c8ef746ffff57214133da7785c61fd1c35da1
br0steen <osteensco@gmail.com>
2021-10-20 21:35:46 -0400

    sticky nav bar bb

--- my_portfolio_website/main_app/views.py
+++ my_portfolio_website/main_app/views.py
@@ -3,5 +3,6 @@ from django.http import HttpResponse
 
 
 def home(response):
-    return render(response, "home.html", {})#dictionary is used for passing things to html code
+    title = '''Scott Osteen's Portfolio'''
+    return render(response, "home.html", {'title': title})#dictionary is used for passing things to html code
 
