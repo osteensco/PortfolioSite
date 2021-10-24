a406ea3fa2d399ff63bed7268846504d3ba1beed
br0steen <osteensco@gmail.com>
2021-10-24 18:43:16 -0400

    added tables to db and admin dashboard

--- my_portfolio_website/main_app/admin.py
+++ my_portfolio_website/main_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Technology, Project
 # Register your models here.
+admin.site.register(Technology)
+admin.site.register(Project)
--- my_portfolio_website/main_app/models.py
+++ my_portfolio_website/main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Technology(models.Model):
+    name = models.CharField(max_length=100)
+    icon = models.ImageField()
+
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=100)
+    short_desc = models.CharField(max_length=280)
+    technologies = models.ManyToManyField(Technology)
+    picture = models.ImageField()
