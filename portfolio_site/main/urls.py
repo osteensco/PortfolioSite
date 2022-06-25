5f022b3818df08f79463e718ab57d005329513b1
br0steen <osteensco@gmail.com>
2022-06-25 17:26:05 -0400

    added urls, views, templates and statics

--- portfolio_site/main/urls.py
+++ portfolio_site/main/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("home", views.home, name="home")
+    path("home", views.home, name="home"),
+    path("<int:id>", views.tech_page, name="tech_page"),
+    path("<str:name>", views.proj_page, name="proj_page"),
 ]
--- portfolio_site/main/views.py
+++ portfolio_site/main/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .models import Technology, Project
 
 
 
 def home(response):
-    return HttpResponse("<h1>Home Page</h1>")
+    title = '''Scott Osteen's Portfolio'''
+    projects = Project.objects.order_by('relevance')
+    techs = Technology.objects.order_by('relevance')
+
+    return render(response, 'home.html', {'title': title, 'projects': projects, 'techs': techs})#dictionary is used for passing things to html code
+
+
+def proj_page(response, name):
+    project = Project.objects.get(name=name)
+    title = project.name
+
+    return render(response, project.html, {'title': title, 'project': project})
+
+
+def tech_page(response, id):
+    tech = Technology.objects.get(id=id)
+    projects = Project.objects.order_by('relevance')
+    title = tech.name
+
+    return render(response, 'basetech.html', {'title': title, 'tech': tech, 'projects': projects})
