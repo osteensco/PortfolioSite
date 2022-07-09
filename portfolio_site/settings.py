00f9a4e7dd77e17a63f0dec6f12145e095f40578
br0steen <osteensco@gmail.com>
2022-07-09 10:34:59 -0400

    Revert "added webhook for GCS"
    This reverts commit 83673f61ca713ef87d82c93eccbeaad54e406641.

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -58,7 +58,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'log_request_id.middleware.RequestIDMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -66,7 +65,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'portfolio_site.middleware.GCS',
 ]
 
 ROOT_URLCONF = 'portfolio_site.urls'
@@ -136,40 +134,8 @@ USE_TZ = True
 
 
 
-# Support for X-Request-ID
 
-LOG_REQUEST_ID_HEADER = 'HTTP_X_REQUEST_ID'
-LOG_REQUESTS = True
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'request_id': {
-            '()': 'log_request_id.filters.RequestIDFilter'
-        }
-    },
-    'formatters': {
-        'standard': {
-            'format': '%(levelname)-8s [%(asctime)s] [%(request_id)s] %(name)s: %(message)s'
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'filters': ['request_id'],
-            'formatter': 'standard',
-        },
-    },
-    'loggers': {
-        'log_request_id.middleware': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    }
-}
 
 
 
