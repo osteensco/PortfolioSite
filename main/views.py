03e0dc130bb8f345a1b2c39dcb0ea2b30c66a3e1
br0steen <osteensco@gmail.com>
2022-07-10 15:32:00 -0400

    cleaned up views and fixed made using btn group

--- main/views.py
+++ main/views.py
@@ -2,29 +2,45 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Technology, Project, Resume
 
-#more akin to request handlers, these functions provide a response from a request. depends upon hit in urls list.
 
+
+
+#helper functions
+def set_tech_dict(projects):
+    dt = {}
+    for project in projects:#manifest list of technologies used for each project
+        l = [tech for tech in project.technologies.order_by('relevance')]
+        d = {f'''{project.name}''': l}
+        dt = dt | d
+
+    return dt
+
+def set_resume():
+    try:
+        r = Resume.objects.get(current=True)
+    except Resume.DoesNotExist:
+        r = None
+    
+    return r
+
+
+
+
+#view functions
+#more akin to request handlers, these functions provide a response from a request. depends upon hit in urls list.
 def home(response):#provides home page, extended from base. includes all projects and technologies
     title = '''Scott Osteen's Portfolio'''
     projects = Project.objects.order_by('relevance')
     techs = Technology.objects.order_by('relevance')
-    try:
-        resume = Resume.objects.get(current=True)
-    except Resume.DoesNotExist:
-        resume = None
-    
-    displaytechs = {}
-    for project in projects:#manifest list of technologies used for each project
-        l = [tech for tech in project.technologies.order_by('relevance')]
-        d = {f'''{project.name}''': l}
-        displaytechs = displaytechs | d
+    display_techs = set_tech_dict(projects)
+    resume = set_resume()
 
     mapping = {
         'title': title,
         'projects': projects,
         'techs': techs,
         'resume': resume,
-        'displaytechs': displaytechs
+        'display_techs': display_techs
         }#dictionary is used for passing things to html code
 
     return render(response, 'home.html', mapping)
@@ -35,16 +51,12 @@ def proj_page(response, name):#queries db for project name and returns appropria
     title = project.name
     techs = project.technologies.order_by('relevance')
     visual_aids = {aid.name: aid.img for aid in project.visual_aids.all()}
-    try:
-        resume = Resume.objects.get(current=True)
-    except Resume.DoesNotExist:
-        resume = None
+    resume = set_resume()
 
     mapping = {
         'title': title,
         'project': project,
         'techs': techs,
-        
         'resume': resume
         } | visual_aids
 
@@ -55,16 +67,15 @@ def tech_page(response, name):#each tech page is extended from basetech.html
     tech = Technology.objects.get(name=name)
     projects = Project.objects.order_by('relevance')
     title = tech.name
-    try:
-        resume = Resume.objects.get(current=True)
-    except Resume.DoesNotExist:
-        resume = None
+    display_techs = set_tech_dict(projects)
+    resume = set_resume()
 
     mapping = {
         'title': title,
         'tech': tech,
         'projects': projects,
-        'resume': resume
+        'resume': resume,
+        'display_techs': display_techs
         }
 
     return render(response, 'basetech.html', mapping)
